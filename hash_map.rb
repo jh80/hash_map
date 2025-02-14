6db99877a5dd648c61dddc32aae5e5c37f34e74a
Jennifer H <74622778+jh80@users.noreply.github.com>
2025-02-14 09:16:11 -0800

    Refactor setting a key/value to a hash map

--- hash_map.rb
+++ hash_map.rb
@@ -23,18 +23,21 @@ class HashMap
 
   def set(key, value)
     index = hash(key) % @capacity
-    if !@map[index]
-      list = LinkedList.new
-      list.append(key, value)
-      @map[index] = list
-      return
-    end
-    l_i = @map[index].find_k(key)
-    if l_i
+    if @map[index].nil?
+      @map[index] = start_list(key, value)
+    elsif (l_i = @map[index].find_k(key)).nil?
+      @map[index].append(key, value)
+    else
       node = @map[index].at(l_i)
       node.value = value
-    else
-      @map[index].append(key, value)
     end
+  end
+
+  private
+
+  def start_list(key, value)
+    list = LinkedList.new
+    list.append(key, value)
+    list
   end
 end
